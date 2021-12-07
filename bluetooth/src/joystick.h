68878d5bd06c93cc6cd95efd0edfc6d85eb63081
GAUTIER Thierry <thierry.gautier44@gmail.com>
2021-12-07 21:59:03 +0100

    Delete bluetooth directory

--- bluetooth/src/joystick.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef JOYSTICK_H_
-#define JOYSTICK_H_
-
-
-/* thread in charge to get joystick order to control ROBOT */
-void* ThreadJoystick(void *arg);
-
-#endif /* JOYSTICK_H_ */
--- bluetooth/src/keyboard.h
+++
@@ -1,7 +0,0 @@
-#ifndef KEYBOARD_H_
-#define KEYBOARD_H_
-
-/** thread in charge to get key board order to control ROBOT */
-void* ThreadKeyBoard(void *arg);
-
-#endif /* KEYBOARD_H_ */
